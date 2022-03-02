8a5243d40de230d9346761cf56d8ddd1c10054ab
breno12filipe <breno.filipe@sempreceub.com>
2022-03-01 21:08:01 -0300

    unindo duas classes vehicle e vehicleops deixando apenas vehicleops

--- api.py
+++ api.py
@@ -9,15 +9,13 @@ app = Flask(__name__)
 api = Api(app)
 prkLt = ParkingLot()
 
-# TODO: Estudar a junção das duas classes Vehicle() e VehicleOps()
-# TODO: Existe uma redundância de nomes entre as classes Vehicle(src.vehicle) e Vehicle(api.py)
-# https://flask-restful.readthedocs.io/en/latest/quickstart.html#endpoints
-class Vehicle(Resource):
+# NOTE: Classes must follow Pascal case
+class VehicleOps(Resource):
     # Post method is equivalent to park vehicle 
     def post(self):
         data = request.json
         try:
-        # TODO: Verificar se a placa já não está registrada e o cpf também
+            # TODO: Verificar se a placa já não está registrada e o cpf também
             if prkLt.verifyVacancy(data['vacancy'], data['vacancy_floor']):
                 if prkLt.verifyParkingLot(data['vacancy']):
                     if prkLt.verifyValet(data['valet_name']):
@@ -40,20 +38,18 @@ class Vehicle(Resource):
         
     # NOTE: Returns all vehicles
     def get(self):
-        return [
+            return [
             {'cpf':vehicle._cpf,
-             'license_plate':vehicle._license_plate,
-             'vacancy_floor': vehicle.vacancy_floor,
-             'vacancy': vehicle.vacancy,
-             'valet_name': vehicle.valet_name,
-             'parking_time': str(vehicle.parking_time),
-             'parking_time_value': vehicle.parking_time_value,
-             'ticket': vehicle.ticket
+            'license_plate':vehicle._license_plate,
+            'vacancy_floor': vehicle.vacancy_floor,
+            'vacancy': vehicle.vacancy,
+            'valet_name': vehicle.valet_name,
+            'parking_time': str(vehicle.parking_time),
+            'parking_time_value': vehicle.parking_time_value,
+            'ticket': vehicle.ticket
             }
             for vehicle in prkLt.parking_lot_vacancies]
-            
-# NOTE: Classes must follow Pascal case
-class VehicleOps(Resource):
+        
     # delete method is equivalent to release vehicle
     def delete(self, ticket):
         for vehicle in prkLt.parking_lot_vacancies:
@@ -66,8 +62,7 @@ class VehicleOps(Resource):
                 return 'Invalid Ticket'
     
 # NOTE: Resource names (URN) must follow kebab-case
-api.add_resource(Vehicle, '/vehicles')
-api.add_resource(VehicleOps, '/vehicle-operations/<string:ticket>')
+api.add_resource(VehicleOps, '/vehicles', '/vehicles/<string:ticket>')
 
 if __name__ == '__main__':
     app.run(debug=True)
